1135c56d7c9666d04fa9f8c62bf5e8b57bcbf366
noonu <astrophysicsandpython@gmail.com>
2022-02-24 13:37:25 +0500

    reverted versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
         name='npy_reader',
-        version='1.0.1',
+        version='1.0.0',
         packages=find_packages(where="src"),
         url='https://github.com/AstrophysicsAndPython/npy_reader',
         license='MIT',

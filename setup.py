b303a2711390db5e3c003f94e6b7c0c20de803a8
Noonuu <astrophysicsandpython@gmail.com>
2022-02-25 02:03:14 +0500

    pypi uploading fix
    * changed the version number because v1.0.0 and v1.0.1 were used during testing previously.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
         name='npy_reader',
-        version='1.0.0',
+        version='1.0.2',
         packages=find_packages(where="src"),
         url='https://github.com/AstrophysicsAndPython/npy_reader',
         license='MIT',

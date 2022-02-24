d6d42261474c1d9a975119137eb09d87bcae721a
noonu <astrophysicsandpython@gmail.com>
2022-02-24 11:08:52 +0500

    properly working gui for npy_reader, moved the functions to a separate file, and moved all the stuff to src folder

--- src/npy_reader.py
+++ src/npy_reader.py
@@ -4,13 +4,13 @@ Created on Feb 23 08:59:21 2022
 
 import tkinter as tk
 
+from functions import hide_show_hidden_files, open_file_dialog_box
+
 ###################################################################################
 # Initialization
 ###################################################################################
 
-# dimensions of the GUI window
-from tkinter import filedialog
-
+# set the width and height of the original window
 _w, _h = 250, 100
 # main is the master name in which all work is going to be done
 main = tk.Tk()
@@ -22,24 +22,20 @@ main.geometry(f'{_w}x{_h}')
 # do not let the window be resized
 main.resizable(False, False)
 
+# also do not let it be resized smaller than it already is
+main.minsize(_w, _h)
+
+hide_show_hidden_files(main)
 
 ###################################################################################
-# Functions
-###################################################################################
-
-def open_file_dialog_box():
-    path = filedialog.askopenfile(initialdir='.', title='Select .npy file',
-                                  filetypes=(('npy files', '*.npy'), ("all files", "*.*")))
-
-
-###################################################################################
-# TK_working
+# TK_decorations
 ###################################################################################
 
 label_file = tk.Label(master=main, text='Browse npy files: ')
 label_file.place(x=_w / 2, y=20, anchor=tk.CENTER)
 
-open_button = tk.Button(master=main, text='Open dialog box', command=open_file_dialog_box)
+# command/lambda taken from https://stackoverflow.com/a/6921225/3212945
+open_button = tk.Button(master=main, text='Open dialog box', command=lambda: open_file_dialog_box(windows=main))
 open_button.place(x=_w / 2, y=60, anchor=tk.CENTER)
 
 main.mainloop()

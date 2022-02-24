efbdebaf47521fdf3c9d43f5bdb3b048ed3b2ca3
noonu <astrophysicsandpython@gmail.com>
2022-02-24 13:33:45 +0500

    minor changes:
    * added long_description, long_description_content_type and classifiers in setup.cfg, and setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ setup(
         author='Astrophysics and Python, Syed Ali Mohsin Bukhari',
         author_email='astrophysicsandpython@gmail.com, syedali.b@outlook.com',
         description='A simple tool to read and display numpy (.npy) files.',
+        long_description='README.md',
+        long_description_content_type="text/markdown",
         python_requires=">=3.7",
-        install_requires="numpy>=1.12.5"
+        install_requires="numpy>=1.12.5",
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3.7"
+            ],
         )

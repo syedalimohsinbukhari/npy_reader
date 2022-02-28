daff11f6ae0bd1eadf301e0e9a4d65f6a8f97be9
noonu <astrophysicsandpython@gmail.com>
2022-02-28 11:09:43 +0500

    versioning fix:
    * changed from v1.0.2 to v1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
         name='npy_reader',
-        version='1.0.2',
+        version='1.0.3',
         packages=find_packages(where="src"),
         url='https://github.com/AstrophysicsAndPython/npy_reader',
         license='MIT',

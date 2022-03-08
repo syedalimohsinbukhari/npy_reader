74bf3787e95cb1924df6d093f41a247f791a658a
Noonuu <astrophysicsandpython@gmail.com>
2022-03-09 01:28:28 +0500

    major changes
    * changed from Tkinter to PySimpleGui
    * reworked the npy_reader.py
    * changed version from 1.0.3 to 1.1.0 in setup.cfg, setup.py
    * updated requirements.txt, setup.cfg, and setup.py
    * added python-check.yml, to just check on push

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setup(
         name='npy_reader',
-        version='1.0.3',
+        version='1.1.0',
         packages=find_packages(where="src"),
         url='https://github.com/AstrophysicsAndPython/npy_reader',
         license='MIT',
         author='Astrophysics and Python, Syed Ali Mohsin Bukhari',
         author_email='astrophysicsandpython@gmail.com, syedali.b@outlook.com',
         description='A simple tool to read and display numpy (.npy) files.',
-        long_description='README.md',
+        long_description=readme,
         long_description_content_type="text/markdown",
         python_requires=">=3.7",
-        install_requires="numpy>=1.12.5",
+        install_requires=["numpy>=1.12.5", "pysimplegui>=4.57.0"],
         classifiers=[
             "License :: OSI Approved :: MIT License",
             "Programming Language :: Python :: 3.7"

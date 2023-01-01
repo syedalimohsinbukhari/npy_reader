0e34f38330550e6ca6c583ca453d5cdbb7004dea
Astrophysics And Python <astrophysicsandpython@gmail.com>
2023-01-01 10:21:50 +0500

    [MAJOR-UPDATE]
    * updated the version for application
    * added memory mapping for reading numpy file
    * added expand_x and expand_y so that the table expands with the windows

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open('README.md', 'r') as f:
     readme = f.read()
 
 setup(
-        name='npy_reader',
-        version='1.1.1',
-        packages=find_packages(where="src"),
-        url='https://github.com/AstrophysicsAndPython/npy_reader',
-        license='MIT',
-        author='Astrophysics and Python, Syed Ali Mohsin Bukhari',
-        author_email='astrophysicsandpython@gmail.com, syedali.b@outlook.com',
-        description='A simple tool to read and display numpy (.npy) files.',
-        long_description=readme,
-        long_description_content_type="text/markdown",
-        python_requires=">=3.7",
-        install_requires=["numpy>=1.12.5", "PySimpleGUI>=4.57.0"],
+    name='npy_reader',
+    version='1.1.2',
+    packages=find_packages(where="src"),
+    url='https://github.com/AstrophysicsAndPython/npy_reader',
+    license='MIT',
+    author='Astrophysics and Python, Syed Ali Mohsin Bukhari',
+    author_email='astrophysicsandpython@gmail.com, syedali.b@outlook.com',
+    description='A simple tool to read and display numpy (.npy) files.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    install_requires=["numpy>=1.12.5", "PySimpleGUI>=4.57.0"],
         classifiers=[
             "License :: OSI Approved :: MIT License",
             "Programming Language :: Python :: 3.7"
--- src/npy_reader/npy_reader.py
+++ src/npy_reader/npy_reader.py
@@ -2,30 +2,50 @@
 Created on Feb 23 08:59:21 2022
 """
 
-import PySimpleGUI as sg
+import os
+import sys
+
 import numpy as np
+import PySimpleGUI as pySG
 
 
-def main():
-    filename = sg.popup_get_file(message='Browse to the .npy file you want to read!',
-                                 title='.npy file reader',
-                                 file_types=(('NumPy File', '.npy'),))
+def npy_reader():
+    """
+    Main function to read the npy files.
+    """
+    # Get the file to read from the user
+    filename = pySG.popup_get_file(message='Browse to the .npy file you want to read!', title='.npy file reader',
+                                   file_types=(('NumPy File', '.npy'),))
 
-    fn = filename.split('/')[-1]
-    _npy = np.load(filename, allow_pickle=True)
+    # Get the file name from the file path
+    fn = os.path.basename(filename)
 
+    # Load the NumPy array from the file,
+    # memory mapping it to allow access to the data without reading it all into memory
+    _npy = np.load(filename, allow_pickle=True, mmap_mode='r')
+    # Generate a list of headings for the table
     heading_list = [f'V{i}' for i in range(len(_npy[0]))]
 
-    layout = [[sg.Table(values=_npy.tolist(),
-                        headings=heading_list,
-                        auto_size_columns=True,
-                        display_row_numbers=True,
-                        num_rows=50)]]
+    # Create the layout for the window
+    layout = [[pySG.Table(values=_npy.tolist(), headings=heading_list, auto_size_columns=True, display_row_numbers=True,
+                          num_rows=50, pad=5, expand_x=True, expand_y=True)]]
 
-    window = sg.Window(title=fn, layout=layout, resizable=True, auto_size_buttons=True, grab_anywhere=False, finalize=True)
+    # Create the window and display it to the user
+    window = pySG.Window(size=(1000, 600), title=fn, layout=layout, resizable=True, auto_size_buttons=True,
+                         grab_anywhere=False)
     window.read()
     window.close()
 
 
+def main():
+    try:
+        npy_reader()
+    except TypeError:
+        pass
+
+
 if __name__ == '__main__':
-    main()
+    try:
+        sys.exit(main())
+    except TypeError:
+        pass
